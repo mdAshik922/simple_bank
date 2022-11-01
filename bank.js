93a30887112384977b7cab83bd8d08dce043d673
mdAshik922 <md.ashik0005723@gmail.com>
2022-11-01 20:49:22 +0600

    rowJavaScript

--- bank.js
+++ bank.js
@@ -3,12 +3,35 @@
   
     // and give it some content
     const sames = document.getElementsByClassName('smae');
-  
+
 for (const same of sames) {
     same.style.backgroundColor = "rgb(133, 158, 211)";
     same.style.width = '150px';
     }
 
+document.getElementById('total_Balance').style.backgroundColor = 'light-blue';
+document.getElementById('total_Balance').style.width = '150px';
+const p_ElementCreate = document.createElement('p');
+const p_ElementCreate_Next = document.createElement('p');
+const span_Create = document.createElement('span');
+span_Create.id = 'balanceDollar';
+
+p_ElementCreate.innerHTML = 'Balance';
+p_ElementCreate.style.textAlign = 'center';
+p_ElementCreate.style.fontSize = 'large';
+
+p_ElementCreate_Next.innerHTML = '$';
+p_ElementCreate_Next.style.textAlign = 'center';
+p_ElementCreate_Next.style.fontSize = 'large';
+
+
+span_Create.innerHTML = '00';
+span_Create.style.textAlign = 'center';
+span_Create.style.fontSize = 'large';
+
+document.getElementById('total_Balance').appendChild(p_ElementCreate);
+document.getElementById('total_Balance').appendChild(p_ElementCreate_Next);
+p_ElementCreate_Next.appendChild(span_Create);
 
 
 
